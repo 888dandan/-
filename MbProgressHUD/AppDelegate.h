ce0443acdac04ca602ae3152230589d680b197fa
888dandan <1550713714@qq.com>
2018-03-18 21:11:02 +0800

    Initial Commit

---
+++ MbProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MbProgressHUD
+//
+//  Created by 曹轩 on 2018/3/18.
+//  Copyright © 2018年 曹轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
